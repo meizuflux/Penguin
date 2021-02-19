af2fb59b1870e5a1d75421b9490d5ea1a4fd7b34
ppotatoo <rkawas731@protonmail.com>
2021-02-19 19:46:39 +0000

    updates

--- cogs/useful.py
+++ cogs/useful.py
@@ -2,6 +2,9 @@ import itertools
 import pathlib
 import platform
 import re
+import io
+import os
+import zlib
 import difflib
 
 import discord
@@ -43,7 +46,7 @@ class Help(commands.MinimalHelpCommand):
     async def send_error_message(self, error):
         ctx = self.context
         destination = self.get_destination()
-        embed = discord.Embed(description=error, color=0x9c5cb4, timestamp=ctx.message.created_at).set_footer(
+        embed = discord.Embed(description=error, color=ctx.bot.embed_color, timestamp=ctx.message.created_at).set_footer(
             text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         await destination.send(embed=embed)
 
@@ -172,12 +175,44 @@ class Help(commands.MinimalHelpCommand):
         match = "\n".join(matches[:1])
         return f"No command called `{string}` found. Did you mean `{match}`?"
 
+class SphinxObjectFileReader:
+    # Inspired by Sphinx's InventoryFileReader
+    BUFSIZE = 16 * 1024
+
+    def __init__(self, buffer):
+        self.stream = io.BytesIO(buffer)
+
+    def readline(self):
+        return self.stream.readline().decode('utf-8')
+
+    def skipline(self):
+        self.stream.readline()
+
+    def read_compressed_chunks(self):
+        decompressor = zlib.decompressobj()
+        while True:
+            chunk = self.stream.read(self.BUFSIZE)
+            if len(chunk) == 0:
+                break
+            yield decompressor.decompress(chunk)
+        yield decompressor.flush()
+
+    def read_compressed_lines(self):
+        buf = b''
+        for chunk in self.read_compressed_chunks():
+            buf += chunk
+            pos = buf.find(b'\n')
+            while pos != -1:
+                yield buf[:pos].decode('utf-8')
+                buf = buf[pos + 1:]
+                pos = buf.find(b'\n')
+
 
 class Useful(commands.Cog, command_attrs=dict(hidden=False)):
     def __init__(self, bot):
         self.bot = bot
         self._original_help_command = bot.help_command
-        bot.help_command = Help(command_attrs=dict(hidden=False))
+        bot.help_command = Help(command_attrs=dict(hidden=False), aliases=['halp', 'h', 'help_command'], help='Literally shows this message. Jesus, do you really need this?')
         bot.help_command.cog = self
 
     def cog_unload(self):
@@ -221,6 +256,146 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
         emb.add_field(name='Average Member Count', value=f'```{average_members:.0f} members per guild```')
 
         await msg.edit(content=None, embed=emb)
+
+
+    # https://github.com/Rapptz/RoboDanny/blob/1d0ddee9273338a13123117fbad6cac3493c8e7f/cogs/api.py from here till rtfm command
+    def finder(self, text, collection, *, key=None, lazy=True):
+        suggestions = []
+        text = str(text)
+        pat = '.*?'.join(map(re.escape, text))
+        regex = re.compile(pat, flags=re.IGNORECASE)
+        for item in collection:
+            to_search = key(item) if key else item
+            r = regex.search(to_search)
+            if r:
+                suggestions.append((len(r.group()), r.start(), item))
+
+        def sort_key(tup):
+            if key:
+                return tup[0], tup[1], key(tup[2])
+            return tup
+
+        if lazy:
+            return (z for _, _, z in sorted(suggestions, key=sort_key))
+        else:
+            return [z for _, _, z in sorted(suggestions, key=sort_key)]
+
+    def parse_object_inv(self, stream, url):
+        # key: URL
+        # n.b.: key doesn't have `discord` or `discord.ext.commands` namespaces
+        result = {}
+
+        # first line is version info
+        inv_version = stream.readline().rstrip()
+
+        if inv_version != '# Sphinx inventory version 2':
+            raise RuntimeError('Invalid objects.inv file version.')
+
+        # next line is "# Project: <name>"
+        # then after that is "# Version: <version>"
+        projname = stream.readline().rstrip()[11:]
+        version = stream.readline().rstrip()[11:]
+
+        # next line says if it's a zlib header
+        line = stream.readline()
+        if 'zlib' not in line:
+            raise RuntimeError('Invalid objects.inv file, not z-lib compatible.')
+
+        # This code mostly comes from the Sphinx repository.
+        entry_regex = re.compile(r'(?x)(.+?)\s+(\S*:\S*)\s+(-?\d+)\s+(\S+)\s+(.*)')
+        for line in stream.read_compressed_lines():
+            match = entry_regex.match(line.rstrip())
+            if not match:
+                continue
+
+            name, directive, prio, location, dispname = match.groups()
+            domain, _, subdirective = directive.partition(':')
+            if directive == 'py:module' and name in result:
+                # From the Sphinx Repository:
+                # due to a bug in 1.1 and below,
+                # two inventory entries are created
+                # for Python modules, and the first
+                # one is correct
+                continue
+
+            # Most documentation pages have a label
+            if directive == 'std:doc':
+                subdirective = 'label'
+
+            if location.endswith('$'):
+                location = location[:-1] + name
+
+            key = name if dispname == '-' else dispname
+            prefix = f'{subdirective}:' if domain == 'std' else ''
+
+            if projname == 'discord.py':
+                key = key.replace('discord.ext.commands.', '').replace('discord.', '')
+
+            result[f'{prefix}{key}'] = os.path.join(url, location)
+
+        return result
+
+    async def build_rtfm_lookup_table(self, page_types):
+        cache = {}
+        for key, page in page_types.items():
+            sub = cache[key] = {}
+            async with self.bot.session.get(page + '/objects.inv') as resp:
+                if resp.status != 200:
+                    raise RuntimeError('Cannot build rtfm lookup table, try again later.')
+
+                stream = SphinxObjectFileReader(await resp.read())
+                cache[key] = self.parse_object_inv(stream, page)
+
+        self._rtfm_cache = cache
+
+    async def do_rtfm(self, ctx, key, obj):
+        page_types = {
+            'latest': 'https://discordpy.readthedocs.io/en/latest',
+            'latest-jp': 'https://discordpy.readthedocs.io/ja/latest',
+            'python': 'https://docs.python.org/3',
+            'python-jp': 'https://docs.python.org/ja/3',
+        }
+
+        if obj is None:
+            await ctx.send(page_types[key])
+            return
+
+        if not hasattr(self, '_rtfm_cache'):
+            await ctx.trigger_typing()
+            await self.build_rtfm_lookup_table(page_types)
+
+        obj = re.sub(r'^(?:discord\.(?:ext\.)?)?(?:commands\.)?(.+)', r'\1', obj)
+
+        if key.startswith('latest'):
+            # point the abc.Messageable types properly:
+            q = obj.lower()
+            for name in dir(discord.abc.Messageable):
+                if name[0] == '_':
+                    continue
+                if q == name:
+                    obj = f'abc.Messageable.{name}'
+                    break
+
+        cache = list(self._rtfm_cache[key].items())
+        def transform(tup):
+            return tup[0]
+
+        matches = self.finder(obj, cache, key=lambda t: t[0], lazy=False)[:8]
+
+        e = discord.Embed(colour=self.bot.embed_color)
+        if len(matches) == 0:
+            return await ctx.send('Could not find anything. Sorry.')
+
+        e.description = '\n'.join(f'[`{key}`]({url})' for key, url in matches)
+        await ctx.send(embed=e)
+
+    @commands.command(aliases=['rtfd'])
+    async def rtfm(self, ctx, *, obj: str = None):
+        """Gives you a documentation link for a discord.py entity.
+        Events, objects, and functions are all supported through a
+        a cruddy fuzzy algorithm.
+        """
+        await self.do_rtfm(ctx, 'latest', obj)
 
 
 def setup(bot):
