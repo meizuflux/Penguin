1377e60bb629169733f4157c16cf8d8dc38d6a68
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-02-21 12:32:02 -0500

    add alex

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands, flags
 from io import BytesIO
+import alexflipnote
 
 
 class Fun(commands.Cog):
     """For the fun commands"""
     def __init__(self, bot):
         self.bot = bot
+        self.bot.alex = alexflipnote.Client(self.bot.config['alex_api_key'])
 
     @commands.command(help='Sends a cat for every error code', aliases=['httpcat', 'http_cat'])
     async def http(self, ctx, code=404):
@@ -21,7 +23,7 @@ class Fun(commands.Cog):
     @flags.add_flag("--dark", action='store_true', default=False)
     @flags.add_flag("--light", action='store_true', default=False)
     @flags.command(help='Makes a supreme logo from text')
-    async def fakesupreme(self, ctx, text, **flags):
+    async def supreme(self, ctx, text, **flags):
         await ctx.send(f'{text} {flags["dark"]} {flags["light"]}')
 
 
