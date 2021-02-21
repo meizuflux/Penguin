7c8128341a23893bba969db6fb361eaac357a608
ppotatoo <rkawas731@outlook.com>
2021-02-21 11:46:17 -0500

    testing flags

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, flags
 from io import BytesIO
 
 
@@ -7,6 +7,7 @@ class Fun(commands.Cog):
     """For the fun commands"""
     def __init__(self, bot):
         self.bot = bot
+        self.bot.add_command(self.supreme)
 
     @commands.command(help='Sends a cat for every error code', aliases=['httpcat', 'http_cat'])
     async def http(self, ctx, code=404):
@@ -17,6 +18,11 @@ class Fun(commands.Cog):
         embed.set_image(url=f"attachment://{code}.png")
         embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed, file=discord.File(BytesIO(buffer), filename=f"{code}.png"))
+
+    @flags.add_flag("--dark", action='store_true', default=False)
+    @flags.command(help='Makes a supreme logo from text')
+    async def supreme(self, ctx, text, **flags):
+        await ctx.send('{flags.dark)')
 
 
 def setup(bot):
