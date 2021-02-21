b0e801b89b255a3c1f3f81855c9cb2890bf46b65
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-02-21 12:09:04 -0500

    forgot f string

--- cogs/fun.py
+++ cogs/fun.py
@@ -22,7 +22,7 @@ class Fun(commands.Cog):
     @flags.add_flag("--dark", action='store_true', default=False)
     @flags.command(help='Makes a supreme logo from text')
     async def fakesupreme(self, ctx, text, **flags):
-        await ctx.send('{flags.dark)')
+        await ctx.send(f'{flags.dark)')
 
 
 def setup(bot):
