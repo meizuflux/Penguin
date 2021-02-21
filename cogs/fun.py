cee74d4424ebd83413a40b0eb6f46ab44292d786
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-02-20 21:28:58 -0500

    embed for http

--- cogs/fun.py
+++ cogs/fun.py
@@ -13,8 +13,12 @@ class Fun(commands.Cog):
         async with self.bot.session.get(
                 f"https://http.cat/{code}") as resp:
             buffer = await resp.read()
-        await ctx.send(
-            file=discord.File(BytesIO(buffer), filename=f"{code}.png"))
+        embed = discord.Embed(colour=self.bot.embed_color,
+                              timestamp=ctx.message.created_at)
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_image(url=f"attachment://{code}.png")
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        await ctx.send(file=discord.File(BytesIO(buffer), filename=f"{code}.png"))
 
 
 def setup(bot):
