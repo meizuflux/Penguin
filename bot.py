308291dede27a1ef627ba835fce1adbca59a7705
ppotatoo <rkawas731@protonmail.com>
2021-02-22 19:10:38 +0000

    gotta load dat

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ class SYSTEM32(commands.Bot):
             print("Connecting to Discord ...")
             self.uptime = datetime.datetime.utcnow()
             self.db = pool_pg
-            extensions = ['jishaku', 'cogs.useful', 'cogs.owner', 'cogs.prefixes', 'cogs.economy', 'cogs.errorhandler', 'cogs.fun', 'cogs.socket']
+            extensions = ['jishaku', 'cogs.useful', 'cogs.owner', 'cogs.prefixes', 'cogs.economy', 'cogs.errorhandler', 'cogs.fun', 'cogs.socket', 'cogs.utilites']
             for extension in extensions:
                 self.load_extension(extension)
 
