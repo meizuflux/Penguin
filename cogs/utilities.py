ca5434e54b3e082031e6169f23fc55269703f2d2
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-02-22 18:00:08 -0500

    pep8

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -4,17 +4,21 @@ import datetime
 from utils.default import qembed
 import humanize
 
+
 class DeletedMessage:
     __slots__ = ('author', 'content', 'channel', 'guild', 'created_at', 'deleted_at')
+
     def __init__(self, message):
         self.author = message.author
         self.content = message.content
         self.guild = message.guild
         self.created_at = message.created_at
         self.deleted_at = datetime.datetime.utcnow()
+
 
 class EditedMessage:
     __slots__ = ('author', 'content', 'channel', 'guild', 'created_at', 'deleted_at')
+
     def __init__(self, message):
         self.author = message.author
         self.content = message.content
@@ -22,10 +26,10 @@ class EditedMessage:
         self.created_at = message.created_at
         self.deleted_at = datetime.datetime.utcnow()
 
-class Utilites(commands.Cog):
+
+class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
 
     def deleted_message_for(self, index: int, channel_id: int):
         try:
@@ -49,20 +53,26 @@ class Utilites(commands.Cog):
         except KeyError:
             self.bot.deleted_messages[message.channel.id] = []
             self.bot.deleted_messages[message.channel.id].append(DeletedMessage(message))
-    
+        if isinstance(message, discord.Embed):
+            await message.channel.send('someone deleted an embed OOP')
+
     @commands.group(invoke_without_subcommand=True)
-    async def snipe(self, ctx, index: int=1, channel: discord.TextChannel=None):
+    async def snipe(self, ctx, index: int = 1, channel: discord.TextChannel = None):
         if not channel:
             channel = ctx.channel
-        msg = self.deleted_message_for(index-1, channel.id) 
+        msg = self.deleted_message_for(index - 1, channel.id)
         if not msg:
             return await qembed(ctx, 'Nothing to snipe!')
-        snipe = discord.Embed(title='Content:', description=f'```{msg.content}```', color=self.bot.embed_color, timestamp=ctx.message.created_at)
-        snipe.add_field(name='Message Stats', value=f'**Created At:** {humanize.naturaldelta(msg.created_at-datetime.datetime.utcnow())} ago\n**Deleted At:** {humanize.naturaldelta(msg.deleted_at-datetime.datetime.utcnow())} ago\n**Index:** {index} / {len(self.bot.deleted_messages[channel.id])}')
+        snipe = discord.Embed(title='Content:', description=msg.content, color=self.bot.embed_color,
+                              timestamp=ctx.message.created_at)
+        snipe.add_field(name='Message Stats', value=f"""
+                        **Created At:** {humanize.naturaldelta(msg.created_at - datetime.datetime.utcnow())} ago\n
+                        **Deleted At:** {humanize.naturaldelta(msg.deleted_at - datetime.datetime.utcnow())} ago
+                        \n**Index:** {index} / {len(self.bot.deleted_messages[channel.id])}""")
         snipe.set_author(name=f'{str(msg.author)} said in #{channel.name}:', icon_url=str(msg.author.avatar_url))
         snipe.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=snipe)
 
 
 def setup(bot):
-    bot.add_cog(Utilites(bot))
+    bot.add_cog(Utilities(bot))
