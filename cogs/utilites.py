f17db6921dcda2767b53cc6144311078e28309a5
ppotatoo <rkawas731@protonmail.com>
2021-02-22 21:28:52 +0000

    funne

--- cogs/utilites.py
+++ cogs/utilites.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import time
+import datetime
 from utils.default import qembed
 import humanize
 
@@ -11,7 +11,7 @@ class DeletedMessage:
         self.content = message.content
         self.guild = message.guild
         self.created_at = message.created_at
-        self.deleted_at = time.time()
+        self.deleted_at = datetime.datetime.utcnow()
 
 class EditedMessage:
     __slots__ = ('author', 'content', 'channel', 'guild', 'created_at', 'deleted_at')
@@ -20,7 +20,7 @@ class EditedMessage:
         self.content = message.content
         self.guild = message.guild
         self.created_at = message.created_at
-        self.deleted_at = time.time()
+        self.deleted_at = datetime.datetime.utcnow()
 
 class Utilites(commands.Cog):
     def __init__(self, bot):
@@ -58,7 +58,7 @@ class Utilites(commands.Cog):
         if not msg:
             return await qembed(ctx, 'Nothing to snipe!')
         snipe = discord.Embed(title='Content:', description=f'```{msg.content}```', color=self.bot.embed_color, timestamp=ctx.message.created_at)
-        snipe.add_field(name='Message Stats', value=f'**Created At:** {humanize.naturaldelta(msg.created_at - time.time())}\n**Deleted At:** {humanize.naturaldelta(msg.deleted_at - time.time())}\n**Index:** {index} / {len(self.bot._deleted__messages_[channel.id])}')
+        snipe.add_field(name='Message Stats', value=f'**Created At:** {humanize.naturaldelta(msg.created_at-datetime.datetime.utcnow())}\n**Deleted At:** {humanize.naturaldelta(msg.deleted_at-datetime.datetime.utcnow())}\n**Index:** {index} / {len(self.bot._deleted__messages_[channel.id])}')
         snipe.set_author(name=f'{str(msg.author)} said in #{channel.name}:', icon_url=str(msg.author.avatar_url))
         snipe.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=snipe)
