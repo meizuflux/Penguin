0be85b475ff3494ba92b20264f99512a4d9e257f
ppotatoo <rkawas731@protonmail.com>
2021-02-22 20:17:07 +0000

    I did the funne

--- cogs/utilites.py
+++ cogs/utilites.py
@@ -36,7 +36,7 @@ class Utilites(commands.Cog):
         readable_order = self.bot._deleted__messages_[channel_id]
         #readable_order.reverse
         try:
-            result = readable_order[index]
+            result = readable_order[index-1]
         except KeyError:
             return None
         else:
