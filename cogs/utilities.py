7a73a6a12a111b3b7fd2170e5dac5ff91ea6f4ec
ppotatoo <rkawas731@protonmail.com>
2021-02-23 16:24:06 +0000

    maybe maybe maybe

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -61,7 +61,7 @@ class Utilities(commands.Cog):
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
         if before.channel.id == 381963689470984203:
-            await before.channel.send(before)
+            await before.channel.send(before.content)
         self.bot.edits[before.channel.id].append(EditedMessage(before))
 
     @commands.group(invoke_without_subcommand=True)
