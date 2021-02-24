ca87ec688e4de61a9698d207bf8a8062275ff3bf
ppotatoo <rkawas731@protonmail.com>
2021-02-24 16:51:15 +0000

    test

--- cogs/fun.py
+++ cogs/fun.py
@@ -23,7 +23,7 @@ class Fun(commands.Cog):
     @flags.add_flag("--dark", action='store_true', default=False)
     @flags.add_flag("--light", action='store_true', default=False)
     @flags.add_flag("--text", default="supreme")
-    @flags.command(usage='"supreme" [--dark|--light]')
+    @commands.command(usage='"supreme" [--dark|--light]', cls=flags.FlagCommand)
     async def supreme(self, ctx, **flags):
         """Makes a custom supreme logo
         example: supreme --text "hey guys" --dark"""
