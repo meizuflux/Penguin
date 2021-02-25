9cab7405c29701610e5f76236edca406978d2837
ppotatoo <rkawas731@protonmail.com>
2021-02-25 14:49:26 +0000

    welp lets hope thi works

--- cogs/polaroid.py
+++ cogs/polaroid.py
@@ -9,8 +9,20 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
     def __init__(self, bot):
         self.bot = bot
 
+    async def get_image(self, ctx, image):
+        async with ctx.typing():
+            if ctx.message.attachments:
+                img = polaroid.Image(await ctx.message.attachments[0].read())
+            elif isinstance(image, discord.PartialEmoji):
+                img = polaroid.Image(await image.url.read())
+            else:
+                img = image or ctx.author
+                img = polaroid.Image(await img.avatar_url_as(format="png").read())
+            return img
+        
+
     @executor_function
-    async def image_manip(self, ctx, image, method: str, *args, **kwargs):
+    def image_manip(self, ctx, image: polaroid.Image, method: str, *args, **kwargs):
         async with ctx.typing():
             if ctx.message.attachments:
                 img = polaroid.Image(await ctx.message.attachments[0].read())
@@ -24,20 +36,25 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
             img.resize(500, 500, 1)
             method = getattr(img, method)
             method(*args, **kwargs)
-            file = discord.File(BytesIO(img.save_bytes()),
-                                filename=f"{method}.png")
+            return img
 
-            embed = discord.Embed(description=text,
-                                  colour=self.bot.embed_color,
-                                  timestamp=ctx.message.created_at)
-            embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-            embed.set_image(url=f"attachment://{method}.png")
-            embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed, file=file)
+    async def send_manip(self, ctx, image, method: str, *args, **kwargs):
+        image = await self.get_image(ctx, image)
+        img = await self.image_manip(ctx, image, method='apply_gradient', *args, **kwargs)
+        file = discord.File(BytesIO(img.save_bytes()),
+                            filename=f"{method}.png")
+
+        embed = discord.Embed(description=text,
+                                colour=self.bot.embed_color,
+                                timestamp=ctx.message.created_at)
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+        embed.set_image(url=f"attachment://{method}.png")
+        embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed, file=file)
 
     @commands.command(help='Makes an image rainbowey')
     async def rainbow(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member] = None):
-        await self.image_manip(ctx, image, method='apply_gradient')
+        await self.send_manip(ctx, image, method='apply_gradient')
 
 def setup(bot):
     bot.add_cog(Polaroid(bot))
