f0b0f864a1490a7eb9c34c84e420ffab307fe6c6
ppotatoo <rkawas731@protonmail.com>
2021-02-26 13:42:06 +0000

    poggies

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -41,7 +41,7 @@ class CommandErrorHandler(commands.Cog):
 
         if isinstance(error, commands.CommandNotFound):
             failed_command = re.match(rf"^({ctx.prefix})\s*(.*)", ctx.message.content, flags=re.IGNORECASE).group(2)
-            matches = difflib.get_close_matches(failed_command, self.context.bot.command_list)
+            matches = difflib.get_close_matches(failed_command, self.bot.command_list)
             if not matches:
                 return
             match = "\n".join(matches[:1])
