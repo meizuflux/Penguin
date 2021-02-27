58f7a2c31feb265264583feb2acd6b756f682bb5
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-02-27 12:53:40 -0500

    honk oh yes

--- cogs/useful.py
+++ cogs/useful.py
@@ -541,7 +541,7 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
         resp = await response.read()
         resp = resp.decode('utf-8')
         resp = ast.literal_eval(resp)
-        level = resp["attributeScores"]["TOXICITY"]["summaryScore"]["value"]*10
+        level = resp["attributeScores"]["TOXICITY"]["summaryScore"]["value"]*100
         await ctx.send(f"`{text}` is `{level:.2f}%` toxic.")
 
 
