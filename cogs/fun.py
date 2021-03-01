37c64928220306ee000a623da8e14501860bc62e
ppotatoo <rkawas731@protonmail.com>
2021-03-01 21:24:22 +0000

    luv u

--- cogs/fun.py
+++ cogs/fun.py
@@ -167,7 +167,7 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def ship(self, ctx, user_1: discord.Member, user_2: discord.Member):
-        love = user_1.id + user_2.id / 5 + 100000 - 30**10 * 50
+        love = user_1.id + user_2.id / 5 + 100000
         await ctx.send(love)
         await qembed(ctx, f'I calculate that the love between {user_1.mention} and {user_2.mention} is {str(love)[:2]}%')
 
