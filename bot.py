882df313d749d4f36ed1015c8d47c796194b0238
ppotatoo <rkawas731@protonmail.com>
2021-03-01 15:14:33 +0000

    I can't python

--- bot.py
+++ bot.py
@@ -112,7 +112,7 @@ class Chuck(commands.Bot):
             if isinstance(command, commands.Group):
                 self.command_list.extend(self.create_command_list(command))
 
-    def command_list_make(self)
+    def command_list_make(self):
         for command in self.commands:
             self.command_list.append(str(command))
             self.command_list.extend([alias for alias in command.aliases])
