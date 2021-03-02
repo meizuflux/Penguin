5086155c1f7908a5a0340e7a1c4682f4f60ab000
ppotatoo <rkawas731@outlook.com>
2021-03-02 18:55:21 -0500

    maybe images

--- cogs/polaroid.py
+++ cogs/polaroid.py
@@ -56,59 +56,59 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
         await self.send_manip(ctx, image, method='apply_gradient')
 
     @commands.command(help='like putin')
-    async def wide(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def wide(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='resize', args=(2000, 900, 1))
 
     @commands.command(help='Inverts an image')
-    async def invert(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def invert(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='invert')
 
     @commands.command(help='It\'s like looking in a mirror')
-    async def flip(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def flip(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='fliph')
 
     @commands.command(help='Blurs an image? Duh')
-    async def blur(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def blur(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='box_blur')
 
     @commands.command(help='cursed')
-    async def sobelh(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def sobelh(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='sobel_horizontal')
 
     @commands.command(help='cursed')
-    async def sobelv(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def sobelv(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='sobel_vertical')
 
     @commands.command(help='Decomposes the image')
-    async def decompose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def decompose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='decompose_max')
 
     @commands.command(help='Turns an image black and white')
-    async def grayscale(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def grayscale(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='grayscale')
 
     @commands.command(help='Solarizes an image')
-    async def solarize(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def solarize(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='solarize')
 
     @commands.command(help='Rotates an image sideways')
-    async def sideways(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def sideways(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='rotate90')
 
     @commands.command(help='Rotates an image upsidedown')
-    async def upsidedown(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def upsidedown(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='rotate180')
 
     @commands.command(help='Makes an image monochrome.')
-    async def monochrome(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def monochrome(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='monochrome')
 
     @commands.command(help='Applies an emboss effect to an image.')
-    async def emboss(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def emboss(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='emboss')
 
     @commands.command(help='Applies an edges effect to an image.')
-    async def edges(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def edges(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='edge_detection')
 
     @commands.group(help='Some commands that apply simple filters.')
@@ -117,43 +117,43 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
             await ctx.send_help(str(ctx.command))
 
     @filter.command(help='Applies a rose filter on an image.')
-    async def rose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def rose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["rosetint"])
 
     @filter.command(help='Applies a pink filter to the image.')
-    async def pink(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def pink(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["pastel_pink"])
 
     @filter.command(help='Applies a liquid filter to the image.')
-    async def liquid(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def liquid(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["liquid"])
 
     @filter.command(help='Applies a dramatic filter to the image.')
-    async def dramatic(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def dramatic(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["dramatic"])
 
     @filter.command(help='Applies a firenze filter to the image.')
-    async def firenze(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def firenze(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["firenze"])
 
     @filter.command(help='Applies a golden filter to the image.')
-    async def golden(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def golden(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["golden"])
 
     @filter.command(help='Applies a lix filter to the image.')
-    async def lix(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def lix(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["lix"])
 
     @filter.command(help='Applies a neue filter to the image.')
-    async def neue(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def neue(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["neue"])
 
     @filter.command(help='Applies an obsidian filter to the image.')
-    async def obsidian(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def obsidian(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["obsidian"])
 
     @filter.command(help='Applies a ryo filter to the image.')
-    async def ryo(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User] = None):
+    async def ryo(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
         await self.send_manip(ctx, image, method='filter', args=["ryo"])
 
 
