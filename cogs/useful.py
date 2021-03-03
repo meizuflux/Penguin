383ee44b4f7920cbbfe39419e73a31f7ac1b8002
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-03 18:24:34 -0500

    hm yes

--- cogs/useful.py
+++ cogs/useful.py
@@ -362,6 +362,10 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
 
     @commands.command(help='Pretty-Prints some JSON')
     async def pprint(self, ctx, json: json.loads):
+        stripped_url = str(json).strip("<>")
+        if stripped_url.startswith(('http', 'https', 'www')):
+            async with ctx.bot.session.get(stripped_url) as resp:
+                json = await resp.json()
         lines = json.dumps(json, indent=4).split('\n')
         paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1985)
         for num, line in enumerate(lines, start=1):
