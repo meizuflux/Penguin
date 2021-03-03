b303125a45fae0cbcc6e026454bb6887694111ae
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-03 18:22:36 -0500

    hm yes

--- cogs/useful.py
+++ cogs/useful.py
@@ -1,16 +1,11 @@
 import asyncio
 import datetime
 import difflib
-import io
 import itertools
-import os
-import pathlib
-from jishaku.paginators import PaginatorInterface, WrappedPaginator
-import platform
 import json
-import re
+import pathlib
+import platform
 import time
-import zlib
 from io import BytesIO
 
 import aiohttp
@@ -18,8 +13,9 @@ import discord
 import humanize
 import psutil
 from discord.ext import commands
+from jishaku.paginators import PaginatorInterface, WrappedPaginator
 
-from utils.default import plural, qembed
+from utils.default import escape, plural, qembed
 
 
 class ChuckContext(commands.Context):
@@ -366,7 +362,13 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
 
     @commands.command(help='Pretty-Prints some JSON')
     async def pprint(self, ctx, json: json.loads):
+        lines = json.dumps(json, indent=4).split('\n')
+        paginator = WrappedPaginator(prefix='```py', suffix='```', max_size=1985)
+        for num, line in enumerate(lines, start=1):
+            paginator.add_line(escape(line))
         await ctx.send(json.dumps(json, indent=4))
+        interface = PaginatorInterface(ctx.bot, paginator, owner=ctx.author)
+        await interface.send_to(ctx)
 
 
 def setup(bot):
