8997fc9708dfe25fa5ac045673e9c5764b5df9da
ppotatoo <rkawas731@outlook.com>
2021-03-03 16:48:37 -0500

    hm yes

--- cogs/polaroid_manipulation.py
+++ cogs/polaroid_manipulation.py
@@ -43,8 +43,9 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
         return img
 
     @executor_function
-    def image_manip(self, img, method: str, args: list = None, kwargs: dict = None):
+    def do_polaroid(self, img, method: str, args: list = None, kwargs: dict = None):
         img = polaroid.Image(img)
+        args[0].strip("''")
         if args is None:
             args = []
         if kwargs is None:
@@ -53,12 +54,12 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
         method(*args, **kwargs)
         return img
 
-    async def send_manip(self, ctx, image, method: str, *args, **kwargs):
+    async def send_polaroid(self, ctx, image, method: str, *args, **kwargs):
         try:
             image = await self.get_image(ctx, image)
         except:
             await qembed(ctx, 'Invalid URL provided.')
-        img = await self.image_manip(image, method, *args, **kwargs)
+        img = await self.do_polaroid(image, method, *args, **kwargs)
         file = discord.File(BytesIO(img.save_bytes()),
                             filename=f"{method}.png")
 
@@ -70,67 +71,71 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
 
     @commands.command(help='Makes an image rainbowey')
     async def rainbow(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='apply_gradient')
+        await self.send_polaroid(ctx, image, method='apply_gradient')
+
+    @commands.command(help='Makes an image magiked', aliases=['magic'])
+    async def magik(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
+        await self.send_polaroid(ctx, image, method='liquid_rescale', args=['img.width - 100, img.height - 100'])
 
     @commands.command(help='like putin')
     async def wide(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='resize', args=(2000, 900, 1))
+        await self.send_polaroid(ctx, image, method='resize', args=(2000, 900, 1))
 
     @commands.command(help='Inverts an image')
     async def invert(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='invert')
+        await self.send_polaroid(ctx, image, method='invert')
 
     @commands.command(help='It\'s like looking in a mirror')
     async def flip(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='fliph')
+        await self.send_polaroid(ctx, image, method='fliph')
 
     @commands.command(help='Blurs an image? Duh')
     async def blur(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='box_blur')
+        await self.send_polaroid(ctx, image, method='box_blur')
 
     @commands.command(help='cursed')
     async def sobelh(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='sobel_horizontal')
+        await self.send_polaroid(ctx, image, method='sobel_horizontal')
 
     @commands.command(help='cursed')
     async def sobelv(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='sobel_vertical')
+        await self.send_polaroid(ctx, image, method='sobel_vertical')
 
     @commands.command(help='Decomposes the image')
     async def decompose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='decompose_max')
+        await self.send_polaroid(ctx, image, method='decompose_max')
 
     @commands.command(help='Turns an image black and white')
     async def grayscale(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='grayscale')
+        await self.send_polaroid(ctx, image, method='grayscale')
 
     @commands.command(help='Solarizes an image')
     async def solarize(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='solarize')
+        await self.send_polaroid(ctx, image, method='solarize')
 
     @commands.command(help='Rotates an image sideways')
     async def sideways(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='rotate90')
+        await self.send_polaroid(ctx, image, method='rotate90')
 
     @commands.command(help='Rotates an image upsidedown')
     async def upsidedown(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='rotate180')
+        await self.send_polaroid(ctx, image, method='rotate180')
 
     @commands.command(help='Makes an image monochrome.')
     async def monochrome(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='monochrome')
+        await self.send_polaroid(ctx, image, method='monochrome')
 
     @commands.command(help='Applies an emboss effect to an image.')
     async def emboss(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='emboss')
+        await self.send_polaroid(ctx, image, method='emboss')
 
     @commands.command(help='Applies an edges effect to an image.')
     async def edges(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='edge_detection')
+        await self.send_polaroid(ctx, image, method='edge_detection')
         
     @commands.command(help='Applies an oil effect to an image.')
     async def oil(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='oil', args=[4, 55])
+        await self.send_polaroid(ctx, image, method='oil', args=[4, 55])
 
     @commands.group(help='Some commands that apply simple filters.')
     async def filter(self, ctx):
@@ -139,43 +144,43 @@ class Polaroid(commands.Cog, command_attrs=dict(hidden=False)):
 
     @filter.command(help='Applies a rose filter on an image.')
     async def rose(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["rosetint"])
+        await self.send_polaroid(ctx, image, method='filter', args=["rosetint"])
 
     @filter.command(help='Applies a pink filter to the image.')
     async def pink(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["pastel_pink"])
+        await self.send_polaroid(ctx, image, method='filter', args=["pastel_pink"])
 
     @filter.command(help='Applies a liquid filter to the image.')
     async def liquid(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["liquid"])
+        await self.send_polaroid(ctx, image, method='filter', args=["liquid"])
 
     @filter.command(help='Applies a dramatic filter to the image.')
     async def dramatic(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["dramatic"])
+        await self.send_polaroid(ctx, image, method='filter', args=["dramatic"])
 
     @filter.command(help='Applies a firenze filter to the image.')
     async def firenze(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["firenze"])
+        await self.send_polaroid(ctx, image, method='filter', args=["firenze"])
 
     @filter.command(help='Applies a golden filter to the image.')
     async def golden(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["golden"])
+        await self.send_polaroid(ctx, image, method='filter', args=["golden"])
 
     @filter.command(help='Applies a lix filter to the image.')
     async def lix(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["lix"])
+        await self.send_polaroid(ctx, image, method='filter', args=["lix"])
 
     @filter.command(help='Applies a neue filter to the image.')
     async def neue(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["neue"])
+        await self.send_polaroid(ctx, image, method='filter', args=["neue"])
 
     @filter.command(help='Applies an obsidian filter to the image.')
     async def obsidian(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["obsidian"])
+        await self.send_polaroid(ctx, image, method='filter', args=["obsidian"])
 
     @filter.command(help='Applies a ryo filter to the image.')
     async def ryo(self, ctx, *, image: typing.Union[discord.PartialEmoji, discord.Member, discord.User, str] = None):
-        await self.send_manip(ctx, image, method='filter', args=["ryo"])
+        await self.send_polaroid(ctx, image, method='filter', args=["ryo"])
 
 
 def setup(bot):
