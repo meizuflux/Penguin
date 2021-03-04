bb9e75afc392b9dccbd583e42780b459237bcea4
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-04 04:46:45 +0000

    ncie

--- cogs/economy.py
+++ cogs/economy.py
@@ -282,6 +282,8 @@ class Economy(commands.Cog, command_attrs=dict(hidden=False)):
             if match and match[0] == 'max':
                 await ctx.send('maximum power')
                 amount = math.floor(wallet / price)
+                if amount = 0:
+                    return await ctx.send('You don\'t have enough money to buy a share.')
             else:
                 amount = 1
 
