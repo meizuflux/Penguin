ac98449ab2930c4193681ad33dd5297494115d99
ppotatoo <rkawas731@outlook.com>
2021-03-03 19:21:03 -0500

    hm yes

--- cogs/useful.py
+++ cogs/useful.py
@@ -218,7 +218,6 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
         bot.help_command = Help(command_attrs=dict(hidden=False, aliases=['halp', 'h', 'help_command'],
                                                    help='Literally shows this message. Jesus, do you really need this?'))
         bot.help_command.cog = self
-        self.chuck_context = ChuckContext
 
     def cog_unload(self):
         self.bot.help_command = self._original_help_command
