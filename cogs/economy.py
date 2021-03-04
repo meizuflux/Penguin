e2eaec88e5d98fa6a4d1acddb97d98e9202f69b1
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-04 14:35:04 +0000

    nice

--- cogs/economy.py
+++ cogs/economy.py
@@ -318,14 +318,18 @@ class Economy(commands.Cog, command_attrs=dict(hidden=False)):
         res = await self.bot.db.fetch("SELECT ticker, amount FROM stocks WHERE user_id = $1", user.id)
         if len(res) == 0:
             return await ctx.send(f'{user} has no stocks', allowed_mentions=discord.AllowedMentions().none())
-        await ctx.bot.get_user(ctx.bot.author_id).send([res["ticker"] for res in res])
+        price = [res["ticker"] for res in res]
+        async with self.bot.session.get(f'https://ws-api.iextrading.com/1.0/tops/last?symbols={",".join[price]}') as resp:
+            data: list = await resp.json()
+        await ctx.send(data)
+        price = [data['price'] for data in data]
         headers = list(res[0].keys())
         table = PrettyTable()
         table.field_names = headers
         for record in res:
             lst = list(record)
             table.add_row(lst)
-        table.add_column("Price", [res["ticker"] for res in res])
+        table.add_column("Price", price)
         msg = table.get_string()
         await ctx.send(f"{user}\'s stocks:```\n{msg}\n```", allowed_mentions=discord.AllowedMentions().none())
 
