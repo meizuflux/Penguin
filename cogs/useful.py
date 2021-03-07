bf30820af0259341796ad7992772fc414aba5902
ppotatoo <rkawas731@outlook.com>
2021-03-07 13:03:38 -0500

    better pypi

--- cogs/useful.py
+++ cogs/useful.py
@@ -160,19 +160,19 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
                 return await qembed(ctx, 'Package not found.')
             package = await f.json()
         data = package['info']
-        embed = ctx.embed(title=f"{data.get('name', 'None')} {data.get('version', 'None')}",
-                              url=data.get('project_url', 'None'),
-                              description=data.get('summary', 'None'))
+        embed = ctx.embed(title=f"{data.get('name', 'None provided')} {data.get('version', 'None provided')}",
+                              url=data.get('project_url', 'None provided'),
+                              description=data.get('summary', 'None provided'))
         embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/381963689470984203/814267252437942272/pypi.png')
-        embed.add_field(name='Author Info:', value=f'**Author Name**: {data.get("author", "None")}\n'
-                                                   f'**Author Email**: {data.get("author_email", "None")}')
+        embed.add_field(name='Author Info:', value=f'**Author Name**: {data.get("author", "None provided")}\n'
+                                                   f'**Author Email**: {data.get("author_email", "None provided")}')
 
-        urls = data.get("project_urls", "None")
+        urls = data.get("project_urls", "None provided")
         embed.add_field(name='Package Info:',
                         value=f'**Documentation URL**: {urls.get("Documentation", "None")}\n'
-                              f'**Home Page**: {urls.get("Homepage", "None")}\n'
-                              f'**Keywords**: {data.get("keywords", "None")}\n'
-                              f'**License**: {data.get("license", "None")}\n',
+                              f'**Home Page**: {urls.get("Homepage", "None provided")}\n'
+                              f'**Keywords**: {data.get("keywords", "None provided")}\n'
+                              f'**License**: {data.get("license", "None provided")}\n',
                         inline=False)
         await ctx.send(embed=embed)
 
