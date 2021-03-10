8e67a03e25acb3e85161aa39d0e92e6e43bf0612
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-10 13:45:19 +0000

    hm

--- cogs/useful.py
+++ cogs/useful.py
@@ -106,7 +106,7 @@ class MenuSource(menus.ListPageSource):
         embed = menu.ctx.embed(title="Commands",
                         description=f"Page {menu.current_page + 1}/{self.get_max_pages()}")
 
-        _commands = page.get_commands
+        _commands = [command for command in page.get_commands()]
         dink = "\n".join(add_formatting(menu.ctx, command) for command in _commands if not command.hidden)
         embed.add_field(name=page.qualified_name, value=dink if not page.description else page.description + "\n\n" + dink)
         return embed
