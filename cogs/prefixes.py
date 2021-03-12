d191d4ee091eeb72a2228087d00e2e78104b0e35
ppotatoo <rkawas731@outlook.com>
2021-03-12 20:16:50 +0000

    yeah

--- cogs/prefixes.py
+++ cogs/prefixes.py
@@ -55,7 +55,7 @@ class Prefixes(commands.Cog):
     @mng_gld()
     async def edit(self, ctx, prefix):
         """Edits the prefix being used to invoke the command."""
-        if ctx.prefix == f"<@!{self.bot.user_id}>":
+        if ctx.prefix == f"<@!{self.bot.user.id}>":
             return await ctx.send(embed=ctx.embed(description="Nice try."))
 
 def setup(bot):
