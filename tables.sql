a684ae312af5372b6e46606c0595cfe5d4eae076
ppotatoo <rkawas731@outlook.com>
2021-03-12 15:17:55 +0000

    hm

--- tables.sql
+++ tables.sql
@@ -11,18 +11,18 @@ CREATE TABLE IF NOT EXISTS prefixes (
 CREATE TABLE IF NOT EXISTS economy (
     user_id BIGINT PRIMARY KEY,
     wallet BIGINT,
-    BANK BIGINT,
+    BANK BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS stocks (
-    user_id BIGINT
-    ticker VARCHAR
-    amount BIGINT
+    user_id BIGINT,
+    ticker VARCHAR,
+    amount BIGINT,
     PRIMARY KEY (user_id, ticker)
 );
 
 CREATE TABLE IF NOT EXISTS todos (
-    user_id BIGINT
-    todo VARCHAR
-    sort_date TIMESTAMP
+    user_id BIGINT,
+    todo VARCHAR,
+    sort_date DATE
 )
