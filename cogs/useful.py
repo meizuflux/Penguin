45366ab985376dbff015a6b18313b756b755fd9f
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-15 14:14:18 +0000

    hm

--- cogs/useful.py
+++ cogs/useful.py
@@ -310,7 +310,7 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
     async def add(self, ctx, *, task: str):
         sql = (
             "INSERT INTO TODOS (user_id, todo, sort_date, jump_url, time) "
-            "VALUES ($1, $2, $3, $4, $4)"
+            "VALUES ($1, $2, $3, $4, $3)"
         )
         await self.bot.db.execute(sql, ctx.author.id, task, datetime.datetime.utcnow(), ctx.message.jump_url)
         await ctx.send(embed=ctx.embed(title="Inserted into your todo list...", description=task))
