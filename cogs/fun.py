c1a6da1d77e1a70888054a20c5f66b56e4f2b1f9
ppotatoo <rkawas731@outlook.com>
2021-03-16 16:02:14 +0000

    why no code

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,6 +8,7 @@ import time
 from io import BytesIO
 
 import discord
+from bs4 import BeautifulSoup
 import typing
 import asyncio
 import json
@@ -414,6 +415,16 @@ class Fun(commands.Cog):
             return
         else:
             await message.add_reaction('<:PepePoint:759934591590203423>')
+
+    @commands.command()
+    async def whyarentyoucoding(self, ctx):
+        """Grabs the latest "Why aren't you coding?" comic."""
+        url = 'https://whyarentyoucoding.com'
+        async with self.bot.session.get(url) as f:
+            data = await f.text()
+        soup = BeautifulSoup(data, 'html.parser')
+        img = url + soup.find_all('img')[1]['src']
+        await ctx.send(embed=ctx.embed(title=str(soup.title.string)).set_image(url=img))
 
 
 def setup(bot):
