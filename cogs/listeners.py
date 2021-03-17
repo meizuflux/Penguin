34b275d6a86e4073ff4da1c133952f4291076f72
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-17 17:41:34 +0000

    hm repl work pls

---
+++ cogs/listeners.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class Listeners(commands.Cog):
+    def __init__(bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        self.bot.usage_counter += 1
+        self.bot.command_usage[ctx.command.qualified_name] += 1
+
+
+
+def setup(bot):
+    bot.add_cog(Listeners(bot))
