d92667a4f4bd740408e9e4125ce3477285958d12
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-18 13:55:25 +0000

    ah

--- cogs/fun.py
+++ cogs/fun.py
@@ -117,7 +117,7 @@ class Fun(commands.Cog):
 
     @morse.command(name='decode', usage='<morse>')
     async def morse_decode(self, ctx, *, morse='.. -.. -.-'):
-        f"""{morse}"""
+        """Decodes a Morse Code string."""
         if morse == "":
             raise commands.BadArgument('You must provide a string of text to translate.')
         translation = ""
