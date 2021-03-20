e323fd06a582d05483961552aebf0878023cd525
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-03-19 22:40:52 -0400

    Update default.py

--- utils/default.py
+++ utils/default.py
@@ -1,6 +1,7 @@
 import traceback
 
 import discord
+from discord.ext import commands
 
 
 def traceback_maker(err, advance: bool = True):
@@ -15,3 +16,6 @@ async def qembed(ctx, text):
     embed = discord.Embed(description=text[:2048], color=bot.embed_color, timestamp=ctx.message.created_at).set_footer(
         text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
     await ctx.send(embed=embed)
+    
+class Maintenence(commands.CheckFailure):
+    pass
