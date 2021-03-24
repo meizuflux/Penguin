081efd7175ac12b4a60dbfdb515c88a0c833bd3f
ppotatoo <rkawas731@outlook.com>
2021-03-24 15:36:46 +0000

    I think I'm dumb

--- cogs/fun.py
+++ cogs/fun.py
@@ -735,9 +735,9 @@ class Fun(commands.Cog):
         if not animes:
             return await ctx.send(embed=ctx.embed(title="Anime not found."))
         result = animes[0]
-        await ctx.author.send(f"```json\n{await ctx.mystbin(json.dumps(result))}```")
+        #await ctx.author.send(f"```json\n{await ctx.mystbin(json.dumps(result))}```")
         synopsis = self.truncate(result['attributes']['synopsis'], 1000)
-        result_embed = ctx.embed(title=result["canonicalTitle"], description=synopsis)
+        result_embed = ctx.embed(title=result["attributes"]["canonicalTitle"], description=synopsis)
         await ctx.send(embed=result_embed)
 
 def setup(bot):
