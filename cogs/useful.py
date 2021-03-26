fdccc68ba50c71991128f2a15bb11cd9fc5f2c2a
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-03-26 15:47:05 -0400

    commit message

--- cogs/useful.py
+++ cogs/useful.py
@@ -350,7 +350,9 @@ class Useful(commands.Cog, command_attrs=dict(hidden=False)):
             "VALUES ($1, $2, $3, $4, $3)"
         )
         await self.bot.db.execute(sql, ctx.author.id, task, datetime.datetime.utcnow(), ctx.message.jump_url)
-        await ctx.send(embed=ctx.embed(title="Inserted into your todo list...", description=task))
+        count = await self.bot.db.fetchval("Select Count(*) from todos where user_id = $1", ctx.author.id)
+
+        await ctx.send(embed=ctx.embed(title="Inserted into your todo list...", description=f"`({count + 1})` " + task))
 
     @todo.command()
     async def remove(self, ctx, numbers: commands.Greedy[int]):
