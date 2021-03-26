925a107f31e7b1d104baeb26fe33c209dee5ec3e
Sitanshu15 <80409424+Sitanshu-15@users.noreply.github.com>
2021-03-26 22:02:06 +0545

    This could be a good fix ..
    Added✅
    Import requests 
    More versatile fact command
    Removed❎
    Not so versatile  fact command

--- cogs/facts.py
+++ cogs/facts.py
@@ -16,73 +16,31 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from discord.ext import commands
+import requests
 
 
 class Facts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
 
-    async def animal_fact(self, ctx, animal):
-        async with self.bot.session.get(f"https://some-random-api.ml/facts/{animal}") as f:
-            data = await f.json()
-        embed = ctx.embed(title="Did you know...", description=data.get("fact", "The API isn't working right now."))
-        await ctx.send(embed=embed)
-
-    @commands.command(aliases=['dogfact'])
-    async def dog_fact(self, ctx):
-        """Sends a dog fact."""
-        await self.animal_fact(ctx, "dog")
-
-    @commands.command(aliases=['catfact'])
-    async def cat_fact(self, ctx):
-        """Sends a cat fact."""
-        await self.animal_fact(ctx, "cat")
-
-    @commands.command(aliases=['pandafact'])
-    async def panda_fact(self, ctx):
-        """Sends a panda fact."""
-        await self.animal_fact(ctx, "panda")
-
-    @commands.command(aliases=['foxfact'])
-    async def fox_fact(self, ctx):
-        """Sends a fox fact."""
-        await self.animal_fact(ctx, "fox")
-
-    @commands.command(aliases=['birbfact'])
-    async def birb_fact(self, ctx):
-        """Sends a birb fact."""
-        await self.animal_fact(ctx, "birb")
-
-    @commands.command(aliases=['koalafact'])
-    async def koala_fact(self, ctx):
-        """Sends a koala fact."""
-        await self.animal_fact(ctx, "koala")
-
-    @commands.command(aliases=['kangaroofact'])
-    async def kangaroo_fact(self, ctx):
-        """Sends a kangaroo fact."""
-        await self.animal_fact(ctx, "kangaroo")
-
-    @commands.command(aliases=['racoonfact'])
-    async def racoon_fact(self, ctx):
-        """Sends a racoon fact."""
-        await self.animal_fact(ctx, "racoon")
-
-    @commands.command(aliases=['elephantfact'])
-    async def elephant_fact(self, ctx):
-        """Sends a elephant fact."""
-        await self.animal_fact(ctx, "elephant")
-
-    @commands.command(aliases=['giraffefact'])
-    async def giraffe_fact(self, ctx):
-        """Sends a giraffe fact."""
-        await self.animal_fact(ctx, "giraffe")
-
-    @commands.command(aliases=['whalefact'])
-    async def whale_fact(self, ctx):
-        """Sends a whale fact."""
-        await self.animal_fact(ctx, "whale")
-
+    @commands.command()
+    async def fact(self, ctx, animal):
+        """Tells you a fact of the given animal parameter"""
+        r = requests.get(f"https://some-random-api.ml/facts/{animal}")
+        res = r.json()
+        fact = res["fact"]
+        factfun = ["Here's a fact", "Here's one", "I bet you dint know"]
+        m = discord.Embed(title=f"{random.choice(factfun)}", description=f"{fact}", color=discord.Color.random())
+        await ctx.send(embed = m)
+        #The help command for this can be available animal's are :-
+        #Dog
+        #Cat
+        #Panda
+        #Fox
+        #Bird
+        #Koala
+        #Whale
 
 def setup(bot):
     bot.add_cog(Facts(bot))
