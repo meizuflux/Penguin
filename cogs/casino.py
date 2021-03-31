fed69eb43b645b78bfa0583e4f236512906735d2
ppotatoo <rkawas731@outlook.com>
2021-03-31 15:39:09 -0400

    e

--- cogs/casino.py
+++ cogs/casino.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
-values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10,
-          'Q': 10, 'K': 10, 'A': 11}
+values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10,
+          'Queen': 10, 'King': 10, 'Ace': 11}
 
 playing = True
 
@@ -33,8 +33,7 @@ class Deck:
         random.shuffle(self.deck)
 
     def deal(self):
-        single_card = self.deck.pop()
-        return single_card
+        return self.deck.pop()
 
 
 class Hand:
