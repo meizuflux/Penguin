7bca5a1c6b3b0f8c1f2b6d6f405e4fa26ebd111f
ppotatoo <rkawas731@outlook.com>
2021-04-02 16:31:43 -0400

    hm

--- utils/eco.py
+++ utils/eco.py
@@ -23,7 +23,16 @@ async def get_stats(ctx, user_id: int, not_author=False):
 
 def get_number(number: str, total: int):
     number = number.replace(",", "")
-    if number.endswith("%"):
+    if "e" in number:
+        if number.replace("e", "").isdigit():
+            parts = number.split("e")
+            try:
+                num = "0" * int(parts[1])
+                amount = parts[0] + num
+            except ValueError:
+                pass
+
+    elif number.endswith("%"):
         number = number.strip("%")
         if not number.isdigit():
             raise commands.BadArgument("That's... not a valid percentage.")
