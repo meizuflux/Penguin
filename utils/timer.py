0cf0e52f6c25b35e3d979fdeefd95c3c55666cd4
ppotatoo <76911441+meizuflux@users.noreply.github.com>
2021-04-06 21:18:26 -0400

    yea boi

---
+++ utils/timer.py
@@ -0,0 +1,34 @@
+import time
+
+
+# This is taken straight from the `?tag timer` created by jay
+class Timer:
+    def __init__(self):
+        self._start = None
+        self._end = None
+
+    def start(self):
+        self._start = time.perf_counter()
+
+    def stop(self):
+        self._end = time.perf_counter()
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+
+    def __int__(self):
+        return round(self.time)
+
+    def __float__(self):
+        return self.time
+
+    def __str__(self):
+        return str(self.__float__())
+
+    @property
+    def time(self):
+        return self._end - self._start
